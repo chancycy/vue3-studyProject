a9d4dd4d96f1ab859cae33c79039cd2134131100
cycy <1531859775@qq.com>
2024-12-27 17:47:33 +0800

    feat: 写自己的app组件

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="app">
+            <h1>hello vue3</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App', // 组件名
+}
+</script>
+
+<style>
+.app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
--- vue3-project/src/main.ts
+++ vue3-project/src/main.ts
@@ -1,6 +1,6 @@
 // 引入createApp用于创建应用--当花盆
 import { createApp } from 'vue';
-// 引入App组件--花的根
+// 引入App组件 app-花的根(以后写的.vue文件都是花的叶)
 import App from './App.vue';
-
-createApp(App).mount('#App')
+// createApp(App)调用时将App传进去并作为根组件-把你的花放进花盆里 mount('#App')挂载在#App上-将你的花盆摆哪
+createApp(App).mount('#App');
